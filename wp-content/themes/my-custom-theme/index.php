0bd3bf4cc14fa6ba1d2b4391333d659e4f95f950
Shlomi Nugarker <shlomin1231@gmail.com>
2023-06-19 02:17:26 +0300

    php function

--- wp-content/themes/my-custom-theme/index.php
+++ wp-content/themes/my-custom-theme/index.php
@@ -1,1 +1,11 @@
-this is my custom theme !!
+<?php
+    function greet($name){
+        echo "<p>Hey $name<p/>";
+    }
+    
+    greet('John');
+    greet('Jane');
+    ?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
