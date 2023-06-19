f74fc2e1e942ef2e4f9ccab3a79a24991b4955da
Shlomi Nugarker <shlomin1231@gmail.com>
2023-06-20 00:27:50 +0300

    statement to get curr page menu

--- wp-content/themes/my-custom-theme/header.php
+++ wp-content/themes/my-custom-theme/header.php
@@ -25,7 +25,13 @@
             ) ?> -->
 
             <ul>
-              <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+              <li <?php
+              if (is_page('about-us') or wp_get_post_parent_id(0) == 7) {
+
+                echo 'class="current-menu-item"';
+              }
+              ?>>
+                <a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
