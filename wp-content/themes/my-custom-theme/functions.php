b2bc5600a7a8a926e9bee84d275a438b34ad7c60
Shlomi Nugarker <shlomin1231@gmail.com>
2023-06-19 17:23:08 +0300

    after_setup_theme action

--- wp-content/themes/my-custom-theme/functions.php
+++ wp-content/themes/my-custom-theme/functions.php
@@ -9,4 +9,10 @@
     }
 
     add_action('wp_enqueue_scripts','getFiles');
+
+    function addFeatures(){
+        add_theme_support('title-tag');
+    };
+
+    add_action('after_setup_theme', 'addFeatures');
    
