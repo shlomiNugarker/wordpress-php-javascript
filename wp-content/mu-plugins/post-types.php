ba0ddc2c18e9cff9d54ccfe0fccb6fc55337faca
Shlomi Nugarker <shlomin1231@gmail.com>
2023-06-21 02:11:18 +0300

    Custom Post Types

---
+++ wp-content/mu-plugins/post-types.php
@@ -0,0 +1,24 @@
+<?php
+
+function post_types()
+{
+    register_post_type(
+        'event',
+        array(
+            'public' => true,
+            'labels' => array(
+                'name' => 'Events',
+                'add_new_item' => 'Add New Event',
+                'edit_item' => 'Edit Event',
+                'all_items' => 'All Events',
+                'singular_name' => 'Event',
+
+
+            ),
+            'menu_icon' => 'dashicon-calendar'
+
+        )
+    );
+}
+
+add_action('init', 'post_types');
