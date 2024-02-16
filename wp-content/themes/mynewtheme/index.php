42083b9f72bc392b4d0044545bab2b7bd6c97d8c
Shlomi Nugarker <shlomin1231@gmail.com>
2024-02-16 14:03:18 +0200

    header & footer

--- wp-content/themes/mynewtheme/index.php
+++ wp-content/themes/mynewtheme/index.php
@@ -1,19 +1,14 @@
-<?php
+<?php get_header();
 
 while (have_posts()) {
     the_post(); ?>
-
-    <h2>
-        <a href="<?php the_permalink() ?>">
+    <h2><a href="<?php the_permalink(); ?>">
             <?php the_title(); ?>
-        </a>
-    </h2>
-
+        </a></h2>
+    <?php the_content(); ?>
     <hr>
+<?php }
 
-    <?php the_content() ?>
-
-    <?php
-}
+get_footer();
 
 ?>
