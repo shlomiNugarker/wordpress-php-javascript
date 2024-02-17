34c41059af3b1e78bf84b07561261dd1e2a62aab
Shlomi Nugarker <shlomin1231@gmail.com>
2024-02-17 16:00:06 +0200

    blog

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -6,16 +6,16 @@
     the_post(); ?>
     
     <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>DONT FORGET TO REPLACE ME LATER</p>
-      </div>
-    </div>  
-  </div>
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>DONT FORGET TO REPLACE ME LATER</p>
+        </div>
+      </div>  
+    </div>
 
-  <div class="container container--narrow page-section">
+    <div class="container container--narrow page-section">
     
     <?php
       $theParent = wp_get_post_parent_id(get_the_ID());
